b5ca0e8b8e9f7f79060c282918d8ab8cab3b311e
Sinashaygan <sinashaygan23041385@gmail.com>
2026-09-04 00:07:57 +0330

    fix:delete the comments

--- src/features/auth/ui/register-form.tsx
+++ src/features/auth/ui/register-form.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { authService } from "@/features/auth/api/auth-service"; // یا @/entities/session/api/auth-service
+import { authService } from "@/features/auth/api/auth-service"; 
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
